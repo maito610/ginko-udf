fdbb4092e379dbef8eeeb554513d86a1ce3e0dd8
Mathieu Dumoulin <mathieu.dumoulin@en-japan.io>
2016-02-01 10:23:49 +0900

    Fist test

--- src/test/java/io/enjapan/il/ginko/CompanySimilarityTest.java
+++ src/test/java/io/enjapan/il/ginko/CompanySimilarityTest.java
@@ -5,6 +5,9 @@ import org.junit.Test;
 
 import java.io.IOException;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
+
 /**
  * Created by dumoulma on 1/25/16.
  */
@@ -15,7 +18,7 @@ public class CompanySimilarityTest {
   @Test
   public void testIdentityUDF() throws IOException {
 
-//    Tuple input = tupleFactory.newTuple(Arrays.asList("a", "b", "c"));
+    //    Tuple input = tupleFactory.newTuple(Arrays.asList("a", "b", "c"));
     //    IdentityUDF func = new IdentityUDF();
     //    Tuple output = func.exec(input);
     //    Assert.assertEquals(input, output);
@@ -23,7 +26,17 @@ public class CompanySimilarityTest {
 
   @Test
   public void testSimScore() throws Exception {
+    CompanyRecord a =
+        CompanyRecord
+            .create(1, "MS", "bill", "1234567", "Tokyo", "", "Shinjuku", "Shinjuku", "1-2-3",
+                    "123456", "www.url.com", true, 30, 1000000);
+    CompanyRecord b =
+        CompanyRecord.create(2, "MSFT", "bill", "1234567", "Tokyo", "", "Shinjuku", "Shinjuku",
+                             "1-2-3", "123456", "url.com", true, 30, 1000000);
 
+    CompanySimilarity udf = new CompanySimilarity();
+    double score = udf.simScore(a, b);
+    assertThat(score, greaterThan(0.0));
   }
 
   @Test
@@ -32,9 +45,3 @@ public class CompanySimilarityTest {
   }
 }
 
-//CompanyRecord a =
-//    CompanyRecord.create("MS", "bill", "1234567", "Tokyo", "", "Shinjuku", "Shinjuku", "1-2-3",
-//                         "123456", true, 30, 1000000);
-//CompanyRecord b =
-//    CompanyRecord.create("MSFT", "bill", "1234567", "Tokyo", "", "Shinjuku", "Shinjuku",
-//                         "1-2-3", "123456", true, 30, 1000000);
