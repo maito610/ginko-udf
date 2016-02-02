6afc67a744aa7434e3ebecc981c6f79ce0e4f56f
maito610 <maito610+gh@gmail.com>
2016-02-02 23:34:42 +0900

    no message

--- src/main/java/io/enjapan/il/ginko/NormalizeNFKC.java
+++ src/main/java/io/enjapan/il/ginko/NormalizeNFKC.java
@@ -1,6 +1,7 @@
 package io.enjapan.il.ginko;
 
 import datafu.pig.util.SimpleEvalFunc;
+import org.apache.pig.data.Tuple;
 import java.text.Normalizer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,16 +9,21 @@ import org.slf4j.LoggerFactory;
 /**
  * Created by mtauchi on 2/2/16.
  */
-public class NormalizeNFKC extends SimpleEvalFunc<String> {
+public class NormalizeNFKC extends SimpleEvalFunc<Tuple> {
   private static final Logger LOG = LoggerFactory.getLogger(TfIdfScore.class);
 
-  public String call(String inputStr) {
-    if (inputStr == null){
+  public String call(Tuple inputStrTuple) {
+    if (inputStrTuple == null || inputStrTuple.size() != 1){
   		return null;
-  	}else{
-  	    String outputStr = Normalizer.normalize(inputStr, Normalizer.Form.NFKC);
-        LOG.debug(String.format("inputStr: %s", inputStr));
-  	    return outputStr;
   	}
+    try{
+      String inputStr = (String)inputStrTuple.get(0);
+      if(inputStr == null){ return null; }
+  	  String outputStr = Normalizer.normalize(inputStr, Normalizer.Form.NFKC);
+      LOG.debug(String.format("inputStr: %s", inputStr));
+  	  return outputStr;
+    } catch (Exception e){
+      return null;
+    }
   }
 }
