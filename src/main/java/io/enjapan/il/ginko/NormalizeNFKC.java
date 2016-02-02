230c31c811b033324a52e02a41e0b14a28f95c99
maito610 <maito610+gh@gmail.com>
2016-02-02 18:48:29 +0900

    no message

--- src/main/java/io/enjapan/il/ginko/NormalizeNFKC.java
+++ src/main/java/io/enjapan/il/ginko/NormalizeNFKC.java
@@ -6,14 +6,18 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Created by mtauchi on 2/2/16. 
+ * Created by mtauchi on 2/2/16.
  */
 public class NormalizeNFKC extends SimpleEvalFunc<String> {
   private static final Logger LOG = LoggerFactory.getLogger(TfIdfScore.class);
 
   public String call(String inputStr) {
-    String outputStr = Normalizer.normalize(inputStr, Normalizer.Form.NFKC);
-    LOG.debug(String.format("inputStr: %s", inputStr));
-    return outputStr;
+    if (inputStr == null){
+  		return null;
+  	}else{
+  	    String outputStr = Normalizer.normalize(inputStr, Normalizer.Form.NFKC);
+        LOG.debug(String.format("inputStr: %s", inputStr));
+  	    return outputStr;
+  	}
   }
 }
